501e1020951a8ba941d50099dc28b500744226dc
Carlos Álvarez <carlos19851@users.noreply.github.com>
2014-04-28 15:16:29 +0000

    Actualización de recursos

---
+++ swbproys/SWPDocumentation/src/org/semanticwb/process/documentation/model/Documentation.java
@@ -0,0 +1,10 @@
+package org.semanticwb.process.documentation.model;
+
+
+public class Documentation extends org.semanticwb.process.documentation.model.base.DocumentationBase 
+{
+    public Documentation(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
